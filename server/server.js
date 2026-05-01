21986e409d9d6fff480542dde15ee5c07e9cbc7b
Anamika Singh <anamikasingh.88891@gmail.com>
2026-05-01 22:43:49 +0530

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,29 +1,28 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectRoutes");
-const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Root Route
 app.get("/", (req, res) => {
   res.send("API Running Successfully");
 });
 
-//app.use("/api/auth", authRoutes);
-//app.use("/api/projects", projectRoutes);
-//app.use("/api/tasks", taskRoutes);
-
+// MongoDB Connect + Start Server
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+.then(() => {
+  console.log("MongoDB Connected");
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.log("Mongo Error:", err);
 });
