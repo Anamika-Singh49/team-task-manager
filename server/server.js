985001e430052c48e72c3925d597d0bb2c9ef31c
Anamika Singh <anamikasingh.88891@gmail.com>
2026-05-01 23:20:58 +0530

    fixed wildcard route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get("/api", (req, res) => {
 });
 
 // React Frontend Route
-app.get("/*", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
