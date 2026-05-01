5174c28c066ea80c1b0242c91eac9555a05263dc
Anamika Singh <anamikasingh.88891@gmail.com>
2026-05-02 01:36:37 +0530

    final express route fix

--- server/server.js
+++ server/server.js
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 //});
 
 // All Other Routes
-app.get("/*", (req, res) => {
-res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 // MongoDB Connect
