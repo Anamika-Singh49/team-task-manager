cdb256ff1d53900023e03ee968998380d367c26e
Anamika Singh <anamikasingh.88891@gmail.com>
2026-05-01 21:28:50 +0530

    fixed root route

--- server/server.js
+++ server/server.js
@@ -11,18 +11,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API Running Successfully");
+});
 
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
+//app.use("/api/auth", authRoutes);
+//app.use("/api/projects", projectRoutes);
+//app.use("/api/tasks", taskRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
-
-app.get("/", (req, res) => {
-  res.send("API Running Successfully");
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
