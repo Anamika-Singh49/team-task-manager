c734c097ebe9bef724b92092427a18e090a76662
Anamika Singh <anamikasingh.88891@gmail.com>
2026-05-01 21:08:09 +0530

    fix port

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
   res.send("API Running Successfully");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
